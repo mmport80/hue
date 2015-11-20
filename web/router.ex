308a53ef4ae3ace1bb927298f3364a3b95c01e08
mmport80 <john.orford@gmail.com>
2015-11-20 19:27:17 +0100

    rss

--- web/router.ex
+++ web/router.ex
@@ -1,26 +1,21 @@
 defmodule Hue2.Router do
-  use Hue2.Web, :router
+        use Hue2.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
+        pipeline :browser do
+                plug :accepts, ["html"]
+                plug :fetch_session
+                plug :fetch_flash
+                plug :protect_from_forgery
+                plug :put_secure_browser_headers
+        end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
-  scope "/", Hue2 do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
-  end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Hue2 do
-  #   pipe_through :api
-  # end
+        scope "/", Hue2 do
+                pipe_through :browser # Use the default browser stack
+                get "/", PageController, :index
+        end
+        
+        scope "/rss", Hue2 do
+           get "/", FeedController, :index
+         end
+ 
 end
