f86d060227940a6ef070881e6e9b44fb098f4754
mmport80 <john.orford@gmail.com>
2016-02-02 11:26:37 +0800

    function breakout

--- web/controllers/feed_controller.ex
+++ web/controllers/feed_controller.ex
@@ -1,14 +1,14 @@
-defmodule Hue2.FeedController do  
+defmodule Hue2.FeedController do
         use Hue2.Web, :controller
         alias Hue2.Article
-        
+
         def index(conn, _params) do
-                articles = Hue2.TweetInfo2.get_articles()
-                
+                articles = Hue2.GetArticles.get_articles()
+
                 conn
                 |> put_layout(:none)
                 |> put_resp_content_type("application/xml")
                 |> render "index.xml", articles: articles
-                
+
         end
 end
